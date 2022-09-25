8ff322550717902cf381aad0469b73da0a934419
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-09-26 00:06:18 +0530

    main window

---
+++ main.py
@@ -0,0 +1,18 @@
+import imp
+from logging import root
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as tk
+
+
+root = Tk()
+root.title("Splashyyy")
+root.geometry("1050x570+150+50")
+root.configure(bg="#f2f3f5")
+root.resizable(False, False)
+
+# TODO: create and insert logo here
+image_icon = PhotoImage(file="")
+
+root.mainloop()
